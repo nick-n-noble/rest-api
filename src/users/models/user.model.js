aa77cd2a842fd4dbcddbaaa3d3a009870afd23ba
Nicholas Noble <nicholas100200@gmail.com>
2021-11-05 16:21:11 -0500

    Updated user model

--- src/users/models/user.model.js
+++ src/users/models/user.model.js
@@ -6,10 +6,7 @@ const userSchema = new Schema(
         userId: { type: String, unique: true, required: true },
         email: { type: String, unique: true, required: true },
         username: { type: String, unique: true, required: true },
-        active: { type: Boolean, default: false },
         password: { type: String, required: true },
-        resetPasswordToken: { type: String, default: null },
-        resetPasswordExpires: { type: Date, default: null },
     },
     {
         timestamps: {
