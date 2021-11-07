0f408c9ab90f48a8fcb16976e8f3655222b6762c
Nicholas Noble <nicholas100200@gmail.com>
2021-11-07 01:39:11 -0500

    Added token comparison with one in database

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,16 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/user.model")
 
 const { TOKEN_KEY } = process.env;
 
-verifyToken = (req, res, next) => {
+verifyToken = async (req, res, next) => {
   const token =
-    req.body.token || req.query.token || req.headers["x-access-token"] || req.cookies["x-access-token"];
+    req.body.token || req.query.token || req.headers["x-access-token"];
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
+  } else if (! (await User.findOne({ token })) ) {
+    return res.status(401).send("Invalid Token");
   }
   try {
     const decoded = jwt.verify(token, TOKEN_KEY);
